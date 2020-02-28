d3bfa2e452610a1d07c543b5dcc05c4057bdb03d
a23246542 <a23246542@gmail.com>
2020-02-29 06:03:08 +0800

    元件沒上傳到

---
+++ src/components/Product.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>123456</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            products:[]
+        }
+    },
+    created(){
+        this.getProduct();
+    },
+    methods:{
+        getProduct(){
+             const api =`${process.env.VUE_APP_API}api/${process.env.VUE_APP_CUSTOMAPI}/admin/products`;
+             const vm =this;
+             this.$http.get(api).then((response)=>{
+             console.log(response.data);
+                if(response.data.success){
+                    vm.products = response.data.products;
+                }
+            })
+        }
+    }
+}
+</script>
